ece062f0dc76d7b9f0caf9a3ce3c17d34c7181f4
Eli Schwartz <eschwartz@archlinux.org>
2019-08-15 09:34:33 -0400

    meson: generate the autotools distribution files
    For cross-compatibility with autotools, a dist tarball created from
    either meson or autotools should have everything needed to build with
    either build system. This ensures that all the files which "make dist"
    would make available, are available.
    
    In order for this to work, autogen.sh needs to know how to enter the
    dist working directory, and fail if it cannot successfully cd or run
    autoreconf. (It's fine if patch fails because the patch was already
    applied.)
    
    Signed-off-by: Eli Schwartz <eschwartz@archlinux.org>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -x
 
-autoreconf -i
+if [ -n "$MESON_DIST_ROOT" ]; then
+	cd "$MESON_DIST_ROOT" || exit 1
+fi
+
+autoreconf -i || exit 1
 patch -d build-aux -Np0 -i ltmain-asneeded.patch
 
 exit 0
